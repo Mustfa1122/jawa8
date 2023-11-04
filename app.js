6e868b311b340057d1df94a18ef86f2c5ea35bae
Mustfa1122 <145039363+Mustfa1122@users.noreply.github.com>
2023-11-04 05:36:49 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var food = prompt("enter your Food")
+if(food === "biryani"){
+    alert("ly ao 1 plate")
+}else if(food === 'burger'){
+    alert("2 burger ly ana")
+}
+else{
+    alert("ghr na ana ")
+}
